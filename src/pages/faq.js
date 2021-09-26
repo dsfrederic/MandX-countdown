0f8b20b4ede13049fae3ce7e495a1e90f92d1d4f
Frédéric De Smet <fredericdesmet@live.be>
2021-09-26 10:17:21 +0200

    Added faq.js

---
+++ src/pages/faq.js
@@ -0,0 +1,32 @@
+import { Divider } from "@theme-ui/components"
+import * as React from "react"
+import Layout  from "../components/layout"
+
+// markup
+const Countdown = () => {
+
+    
+
+  return (
+    <Layout >
+        <h2>Praktische info</h2>
+        <p>Hieronder kan je alvast wat praktische info vinden omtrent ons trouwweekend.</p>
+
+       <span style={{padding: "25px"}}></span>
+        <h4>Bla bla bla </h4>
+        <p>Antwoord</p>
+        <Divider />
+        <h4>Bla bla bla </h4>
+        <p>Antwoord</p>
+        <Divider />
+        <h4>Bla bla bla </h4>
+        <p>Antwoord</p>
+        <Divider />
+        <h4>Bla bla bla </h4>
+        <p>Antwoord</p>
+        <Divider />
+    </Layout>
+  )
+}
+
+export default Countdown
